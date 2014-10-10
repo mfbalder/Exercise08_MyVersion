fbf912b9f905739b5949d4725d529a9687ad5a1a
Micki <mfbalder@gmail.com>
2014-10-09 23:17:11 -0700

    Finished loose functional version of text generator, added jean.txt

--- markov.py
+++ markov.py
@@ -3,50 +3,48 @@
 from sys import argv
 import random
 
-def make_markov_chain(input_text):
-    """Takes an input text as a string and returns a dictionary of
-    markov chains."""
-    # list_of_text = list(input_text)
-    # list_of_text = [line.replace("\n", " ") for line in list_of_text]
-    # print "".join(list_of_text)
+
+def assemble_dictionary(list_of_opened_files):
+    """Takes a list of opened files, cleans them up, and adds them to a dictionary"""
+
+    def clean_file(input_file):
+        cleaned_text = input_file.read().split()
+        for word in cleaned_text:
+            word.strip(',()[]')
+        return cleaned_text
+
+    def add_file_to_dict(cleaned_text):
+        for word_index in range(len(cleaned_text) - 2):
+            key = (cleaned_text[word_index], cleaned_text[word_index + 1])
+            value = cleaned_text[word_index + 2]
+            markov_chain.setdefault(key, []).append(value)
 
     markov_chain = {}
-    text = input_text.read().split()
-    for word_index in range(len(text) - 2):
-        key = (text[word_index], text[word_index + 1])
-        value = text[word_index + 2]
-        markov_chain.setdefault(key, []).append(value)
+    for each_file in list_of_opened_files:
+        cleaned_text = clean_file(each_file)
+        add_file_to_dict(cleaned_text)
     return markov_chain
 
-def make_text(markov_chain):
-    """Takes a dictionary of markov chains and returns random text
-    based off an original text."""
-    # randomly selects a key of words to start with, if the first word starts with a capital letter
-    text_seed = random.choice(filter(lambda x: x[0][0].isupper(), markov_chain.keys()))
-    # print text_seed
-    # start the sentence off, with the first two words
-    sentence = [text_seed[0], text_seed[1]]
-
-    # as long as our key is still in the dictionary
-    while text_seed in markov_chain:
-        if text_seed[1][-1] in ".?!":
-            break
-        # randomly choose the word from the value options
-        next_word = random.choice(markov_chain[text_seed])
-        # add that word to the sentence
+def assemble_text(chain):
+    """Takes a dictionary of markov chains as input, and returns a random text"""
+    start = random.choice(filter(lambda x: x[0][0].isupper(), chain.keys()))
+    sentence = [start[0], start[1]]
+    while start in chain.keys():
+        next_word = random.choice(chain[start])
         sentence.append(next_word)
-        # set the next word pair
-        text_seed = (text_seed[1], next_word)
+        if next_word[-1] in ".?":
+            break
+        start = (sentence[-2], sentence[-1])
+
     return " ".join(sentence)
 
-def main(filename):
 
-    input_text = open(filename)
-
-    chain_dict = make_markov_chain(input_text)
-    random_text = make_text(chain_dict)
-    print random_text
+def main(files):
+    # for each file that the user passes as an argument, open it and process it
+    opened_files = [open(file_name) for file_name in files]
+    markov_chain = assemble_dictionary(opened_files)
+    print assemble_text(markov_chain)
 
 if __name__ == "__main__":
-    script, filename = argv
-    main(filename)
+    files = argv[1:]
+    main(files)
