f15656bfdc852fa7e41e87cc5adb029813c6f6b1
Micki <mfbalder@gmail.com>
2014-10-12 23:01:32 -0700

    Experimented with regex, added a genetics.txt

--- markov.py
+++ markov.py
@@ -2,16 +2,25 @@
 
 from sys import argv
 import random
+import re
 
 
 def assemble_dictionary(list_of_opened_files):
     """Takes a list of opened files, cleans them up, and adds them to a dictionary"""
 
     def clean_file(input_file):
-        cleaned_text = input_file.read().split()
-        for word in cleaned_text:
-            word.strip(',()[]')
+        # text = input_file.read().split()
+        # cleaned_text = [re.sub(r'[,?.]', '', word) for word in text]
+        # return cleaned_text
+        cleaned_text = []
+        text = input_file.read().split()
+        for word in text:
+            word = re.sub(r'[,?.]', '', word)
+            cleaned_text.append(word)
+        # print cleaned_text
         return cleaned_text
+
+
 
     def add_file_to_dict(cleaned_text):
         for word_index in range(len(cleaned_text) - 2):
@@ -27,7 +36,7 @@ def assemble_dictionary(list_of_opened_files):
 
 def assemble_text(chain):
     """Takes a dictionary of markov chains as input, and returns a random text"""
-    start = random.choice(filter(lambda x: x[0][0].isupper(), chain.keys()))
+    start = random.choice(filter(lambda x: x[0][-1] not in ".", chain.keys()))
     sentence = [start[0], start[1]]
     while start in chain.keys():
         next_word = random.choice(chain[start])
@@ -43,7 +52,13 @@ def main(files):
     # for each file that the user passes as an argument, open it and process it
     opened_files = [open(file_name) for file_name in files]
     markov_chain = assemble_dictionary(opened_files)
-    print assemble_text(markov_chain)
+    final_sentence1 = assemble_text(markov_chain)
+    final_sentence2 = assemble_text(markov_chain)
+    while len(final_sentence1 + final_sentence2) > 140:
+        final_sentence1 = assemble_text(markov_chain)
+        final_sentence2 = assemble_text(markov_chain)
+    print final_sentence1[0].upper() + final_sentence1[1:] + " " + final_sentence2[0].upper() + final_sentence2[1:]
+
 
 if __name__ == "__main__":
     files = argv[1:]
